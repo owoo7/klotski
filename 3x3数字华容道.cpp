a9562876facf3cdc28fe5d93e93cd3335cf60bec
owoo7 <69041731+owoo7@users.noreply.github.com>
2020-08-01 11:13:34 +0800

    3x3数字华容道第二代
    改用方向键控制

--- 3x3数字华容道.cpp
+++ 3x3数字华容道.cpp
@@ -1,8 +1,10 @@
 #include<iostream>
+#include<stdio.h>
+#include <conio.h>
 using namespace std;
 int main()
 {	
-	int numx = 2,numy = 2,finnum = 0;
+	int numx = 2,numy = 2,finnum = 0,ch1 = 0,ch2 = 0;
 	char ans;
 	int test[3] [3] =
 	{	
@@ -10,20 +12,22 @@ int main()
 	    {4,6,1},
 		{2,7,0}	
 	};
-	cout<<"You can use key:w a s d to control the game.w for up,a for left,d for right and s for down.zero is impty and you can use it to restore those numbers into right positions."<<"\n";
+	cout<<"You can use direction keys to control the game.Zero is impty and you can use it to restore those numbers into right positions."<<"\n";
 	for(int x = 0;x < 3;x++)
 	{
 		for(int y = 0;y < 3;y++)
 			cout<<"\t"<<test[x][y]<<"\t";
 		cout<<"\n\n\n";
 	};
-	do
-	{
-		cin>>ans;
-		switch(ans)
-		{
-			case 'w': 
-			{
+	while(finnum == 0)
+	{	 
+        if (ch1=getch())
+        { 
+            ch2=getch();
+            switch (ch2)
+            {
+	            case 72:
+		    {
 				if(numx != 2)
 				{
 					int i = test[numx + 1][numy];
@@ -36,9 +40,9 @@ int main()
 					cout<<"Sorry,this operation cannot be performed in this situation."<<"\n";
 				};
 				break;
-			};
-			case 's':
-			{
+		    };
+	            case 80:
+	            {
 				if(numx != 0)
 				{
 					int i = test[numx - 1][numy];
@@ -52,8 +56,8 @@ int main()
 				};
 				break;
 			};
-			case 'a':
-			{
+	            case 75:
+	            {
 				if(numy != 2)
 				{
 					int i = test[numx][numy + 1];
@@ -67,7 +71,7 @@ int main()
 				};
 				break;
 			};
-			case 'd':
+	            case 77:
 			{
 				if(numy != 0)
 				{
@@ -79,11 +83,13 @@ int main()
 				else
 				{
 					cout<<"Sorry,this operation cannot be performed in this situation."<<"\n";
-				};
-				break;
-			};			   
-		};
-		for(int x = 0;x < 3;x++)
+					};
+					break;
+				};			                                     
+	            default: ;break;
+	                break;
+            }
+            for(int x = 0;x < 3;x++)
 		{
 			for(int y = 0;y < 3;y++)
 				cout<<"\t"<<test[x][y]<<"\t";
@@ -92,8 +98,8 @@ int main()
 		if(test[2][2] == 0)
 			if(test[0][0] == 1 && test[0][1] == 2 && test[0][2] == 3 && test[1][0] == 4 && test[1][1] == 5 && test[1][2] == 6 &&test[2][0] == 7 && test[2][1] == 8)
 				finnum = 1;
-	}
-	while(finnum == 0);
+         }  
+	};
 	cout<<"You won,congratulations!";
 	return 0;
 }
